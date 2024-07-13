dcb34b8605c7e437745aa33732fcaea1faaccfa8
valost <valerieostrovska@yahoo.com>
2024-07-13 19:49:43 +0300

    make build

---
+++ index.342efa31.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");n.append(r),Object.keys(t).forEach(function(n){var a=document.createElement("li");a.textContent=n,r.append(a),Object.keys(t[n]).length>0&&e(a,t[n])})}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.342efa31.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    element.append(ul);
+    Object.keys(data).forEach((key)=>{
+        const li = document.createElement("li");
+        li.textContent = key;
+        ul.append(li);
+        if (Object.keys(data[key]).length > 0) createTree(li, data[key]);
+    });
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
